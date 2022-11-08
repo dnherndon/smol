6aaf2520fdef3f0c0b6c263f1861eac2abac4c9e
Nigh Herndon <kf5gsf@gmail.com>
2022-11-08 08:12:05 -0600

    Additions of license headers

--- tokenizer.c
+++ tokenizer.c
@@ -1,3 +1,19 @@
+/*   smol is a small C compiler
+ *   Copyright (C) 2022  D. Nigh Herndon
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "smol.h"
 
 TOKEN* create_token(lexicalElement type, char* beginning, char* end){
@@ -133,6 +149,7 @@ TOKEN* tokenizer (char* scanner){
         || (*scanner == '_')){
             char* p = scanner;
             while(isalnum(*scanner) || (*scanner == '_')) scanner++;
+            
             current = current->next = create_token(IDNTFR, p, scanner);
             continue;
         }
